5cd99e36890041fb1f541af75eb79403e025dc04
chiyoyo2000 <61735023+chiyoyo2000@users.noreply.github.com>
2020-03-03 19:14:00 +0900

    Add files via upload

---
+++ neko.py
@@ -0,0 +1,25 @@
+# discord.pyのインポート
+import discord
+client = discord.Client()
+
+
+# ログイン処理
+@client.event
+async def on_ready():
+    msg = "おいすおいすー"
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+# ねこくんお喋り処理
+@client.event
+async def on_message(message):
+# 発言者がBOTなら無視
+   if message.author.bot:
+       return
+   if message.content == 'ねこぴっぴ':
+       await message.channel.send('なんですか,どーされましたかー？,呼んだ？')
+
+# ねこくんのトークン設定
+client.run("NjgxNzQ3OTk1MTU0NDQ4NDU4.XlS9dA.YurRox1N86zzL7l4hjZGzqdn3gw")
